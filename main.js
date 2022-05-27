ec11d57ec496508d78ecf07229401302df48bc2e
Christopher Liu <christopherliu@sorcero.com>
2022-05-27 11:13:24 -0400

    Create physics engine

---
+++ main.js
@@ -0,0 +1,82 @@
+import { Charge } from "./modules/charge.js"
+
+let c = document.getElementById("canvas");
+let ctx = c.getContext("2d");
+let requestID;
+
+let goButton = document.getElementById("goButton");
+
+let charges = [
+    new Charge(300, 300, 10, 0, 10, 10, "red"),
+    new Charge(300, 500, 10, 0, -10, 10, "blue")
+];
+
+let clear = function () {
+    ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
+}
+
+let drawFrame = function () {
+    window.cancelAnimationFrame(requestID);
+    clear();
+
+    for (let i = 0; i < charges.length; i++) {
+        for (let j = i + 1; j < charges.length; j++) {
+            let q1 = charges[i];
+            let q2 = charges[j];
+
+            let r = Math.sqrt((q1.x - q2.x) ** 2 + (q1.y - q2.y) ** 2);
+            if (r > 30) {
+                continue;
+            }
+
+            let scale1 = (2 * q2.m * ((q1.vx - q2.vx) * (q1.x - q2.x) + (q1.vy - q2.vy) * (q1.y - q2.y))) / ((q1.m + q2.m) * r ** 2);
+            let scale2 = (2 * q1.m * ((q2.vx - q1.vx) * (q2.x - q1.x) + (q2.vy - q1.vy) * (q2.y - q1.y))) / ((q2.m + q1.m) * r ** 2);
+
+            q1.vx = q1.vx - scale1 * (q1.x - q2.x);
+            q1.vy = q1.vy - scale1 * (q1.y - q2.y);
+            q2.vx = q2.vx - scale2 * (q2.x - q1.x);
+            q2.vy = q2.vy - scale2 * (q2.y - q1.y);
+
+            let adj = (31 - r) / (2 * r); // adjustment to prevent overlap
+            q1.x += (q1.x - q2.x) * adj;
+            q1.y += (q1.y - q2.y) * adj;
+            q2.x += (q2.x - q1.x) * adj;
+            q2.y += (q2.y - q1.y) * adj;
+
+        }
+
+        let q = charges[i];
+        if (q.x - 15 <= 0) {
+            q.x = 15;
+            q.vx *= -1;
+        }
+        if (q.x + 15 >= c.clientWidth) {
+            q.x = c.clientWidth - 15;
+            q.vx *= -1;
+        }
+        if (q.y - 15 <= 0) {
+            q.y = 15;
+            q.yx *= -1;
+        }
+        if (q.y + 15 >= c.clientHeight) {
+            q.y = c.clientHeight - 15;
+            q.yx *= -1;
+        }
+    }
+
+
+
+    charges.forEach(charge => {
+        charge.applyEField(ctx, charges);
+        charge.applyBField(ctx, charges);
+    });
+
+    charges.forEach(charge => {
+        charge.update();
+        charge.draw(ctx);
+    })
+
+    requestID = window.requestAnimationFrame(drawFrame);
+}
+
+goButton.addEventListener("click", drawFrame);
---
+++ modules/charge.js
@@ -0,0 +1,127 @@
+export class Charge {
+    constructor(x, y, vx, vy, q, m, color) {
+        this.econst = 10; // electric constant
+        this.bconst = 0.5; // magnetic constant
+
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.q = q;
+        this.m = m;
+        this.color = color;
+
+        this.ax = 0;
+        this.ay = 0;
+    }
+
+    applyEField(ctx, charges) {
+        charges.forEach(charge => {
+            if (charge === this) {
+                return;
+            }
+
+            let rx = charge.x - this.x;
+            let ry = charge.y - this.y;
+            let rnorm = Math.sqrt(rx ** 2 + ry ** 2);
+            let Fx = -(this.econst * this.q * charge.q * rx) / (rnorm ** 3);
+            let Fy = -(this.econst * this.q * charge.q * ry) / (rnorm ** 3);
+
+            // console.log("E", Fx, Fy);
+
+            let vectorConstant = 5000;
+            this.drawVector(ctx, this.x, this.y, this.x + vectorConstant * Fx, this.y + vectorConstant * Fy, 2, "green");
+
+            this.ax += Fx / this.m;
+            this.ay += Fy / this.m;
+        });
+    }
+
+    applyBField(ctx, charges) {
+        charges.forEach(charge => {
+            if (charge === this) {
+                return;
+            }
+
+            let rx = charge.x - this.x;
+            let ry = charge.y - this.y;
+            let rnorm = Math.sqrt(rx ** 2 + ry ** 2);
+
+            let Bz = (this.bconst * charge.q * (charge.vx * ry - charge.vy * rx)) / (rnorm ** 3);
+            let Fx = -this.q * this.vy * Bz;
+            let Fy = this.q * this.vx * Bz;
+
+            // console.log("B", Fx, Fy);
+
+            let vectorConstant = 10000;
+            this.drawVector(ctx, this.x, this.y, this.x + vectorConstant * Fx, this.y + vectorConstant * Fy, 2, "blue");
+
+            this.ax += Fx / this.m;
+            this.ay += Fy / this.m;
+        })
+    }
+
+    drawVector(ctx, fromx, fromy, tox, toy, arrowWidth, color) {
+        // from the internet
+
+        // variables to be used when creating the arrow
+        let headlen = 10;
+        let angle = Math.atan2(toy-fromy,tox-fromx);
+     
+        ctx.save();
+        ctx.strokeStyle = color;
+     
+        // starting path of the arrow from the start square to the end square
+        // and drawing the stroke
+        ctx.beginPath();
+        ctx.moveTo(fromx, fromy);
+        ctx.lineTo(tox, toy);
+        ctx.lineWidth = arrowWidth;
+        ctx.stroke();
+     
+        // starting a new path from the head of the arrow to one of the sides of
+        // the point
+        ctx.beginPath();
+        ctx.moveTo(tox, toy);
+        ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),
+                   toy-headlen*Math.sin(angle-Math.PI/7));
+     
+        // path from the side point of the arrow, to the other side point
+        ctx.lineTo(tox-headlen*Math.cos(angle+Math.PI/7),
+                   toy-headlen*Math.sin(angle+Math.PI/7));
+     
+        // path from the side point back to the tip of the arrow, and then
+        // again to the opposite side point
+        ctx.lineTo(tox, toy);
+        ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),
+                   toy-headlen*Math.sin(angle-Math.PI/7));
+     
+        // draws the paths created above
+        ctx.stroke();
+        ctx.restore();
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 15, 0, 360);
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = "black";
+        ctx.fill();
+        ctx.stroke();
+
+        this.drawVector(ctx, this.x, this.y, this.x + 50 * this.vx, this.y + 50 * this.vy, 2, "red");
+    }
+
+    update() {
+        this.vx += this.ax;
+        this.vy += this.ay;
+
+        this.x += this.vx;
+        this.y += this.vy;
+
+        // console.log(this.x, this.y);
+
+        this.ax = 0;
+        this.ay = 0;
+    }
+}
