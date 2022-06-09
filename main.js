032cc15ec5697022b2597a1c34988f3e78d97aa6
Lucas-Lee11 <lucasclee11@gmail.com>
2022-06-09 14:47:30 -0400

    instructions

--- main.js
+++ main.js
@@ -321,8 +321,16 @@ resetButton.addEventListener("click", resetGame);
 resetLevel();
 saveConfig();
 editGame();
-// swal({
-//     title: `Welcome to ParticleGame!`,
-//     text: "[Instructions]",
-//     button: "Let's go!",
-// });
+swal({
+    title: `Welcome to ParticleGame!`,
+    icon: "info",
+    text: `
+    The goal is to guide your particle (in green) to the target zone (also in green). Depending on the level, there might be other particles (in black) that can be used to help you in your goal.
+
+    On the side in EDIT mode, you can see the physical starting charateristics of the particles, with your particle always first. A movable particle can be dragged around on the canvas while a stationary one cannot. On each level, there may be some charateristics fields that can or cannot be edited. If the velocity is editable, you may also drag around the red velocity vector to edit the particle's velocity
+
+    Once you hit GO, the simulation will start. Particles will enact electric and magnetic forces on each other. Each particle will have several arrows from its center that you can use to help you. The green arrows display the direction and magnitude of electic forces, one for each electric interaction. The same applies for the blue arrows and magnetic forces. The red arrow denotes the direction and magitude of the particle's velocity.
+
+    You can hit EDIT at any time to retry and RESET to restart the level with its initial configurations. Once you guide your green particle to the goal zone, you can move on to the next level. Have fun!`,
+    button: "Play!",
+});
